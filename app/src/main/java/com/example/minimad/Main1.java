df386d29944709fad9a0de4ae2a840c656a702c3
Nithya91532 <nithyadilshani18@gmail.com>
2020-08-17 11:03:01 -0700

    forth commit

---
+++ app/src/main/java/com/example/minimad/Main1.java
@@ -0,0 +1,14 @@
+package com.example.minimad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
